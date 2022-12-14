62d53cb48005a1960a26b41c64806b8a11eeba39
Raphael Machado <raphael.mm.91@gmail.com>
2022-12-13 21:53:08 -0300

    learning sql, docker
    -how to run sqlserver on docker
    -queries with azure data studio
    -how to insert and update data with
    dapper

---
+++ 2805-master/2805-master/README.md
@@ -0,0 +1,14 @@
+# Curso 2805 - Fundamentos do SQL Server
+
+## Instalação
+Para realizar este curso você precisará do Docker e SQL Server instalados na sua máquina.
+
+* [Tutorial - Instalação do Docker](https://balta.io/blog/docker-instalacao-configuracao-e-primeiros-passos?utm_source=github&utm_medium=2805-repo&utm_campaign=readme).
+* [Tutorial - Instalação do SQL Server no Docker](https://balta.io/blog/sql-server-docker?utm_source=github&utm_medium=2805-repo&utm_campaign=readme)
+
+Para acessar o SQL Server instalado com mais facilidade, recomendamos o **Azure Data Studio**
+* [Download do Azure Data Studio](https://docs.microsoft.com/pt-br/sql/azure-data-studio/download-azure-data-studio?view=sql-server-ver15)
+
+## Outros links & material de consulta
+* Tipos de dados no SQL Server ([Documentação oficial](https://docs.microsoft.com/pt-br/sql/t-sql/data-types/data-types-transact-sql?view=sql-server-ver15))
+* Utilizando ```JOINS``` ([Documentação oficial](https://docs.microsoft.com/pt-br/sql/relational-databases/performance/joins?view=sql-server-ver15))
---
+++ access_data/Program.cs
@@ -0,0 +1,78 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BaltaDataAcess.Model;
+using Microsoft.Data.SqlClient;
+using Dapper;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#t;TrustServerCertificate=True;Encrypt=True";
+
+        using (var connection = new SqlConnection(connectionString))
+        {
+            UpdateCategory(connection);
+            ListCategories(connection);
+            // CreateCategory(connection);
+        }
+    }
+    static void ListCategories(SqlConnection connection)
+    {
+        var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+
+        foreach (var item in categories)
+        {
+            Console.WriteLine($"{item.Id} - {item.Title}");
+        }
+    }
+    static void CreateCategory(SqlConnection connection)
+    {
+        var category = new Category();
+        category.Id = Guid.NewGuid();
+        category.Title = "Ammazon AWS";
+        category.Url = "amazon";
+        category.Description = "Categoria destinada a serviços do AWS";
+        category.Order = 8;
+        category.Summary = "AWS Cloud";
+        category.Featured = false;
+
+        // NAO CONCATENAR STRINGS - PARA EVITAR SQL INJECTION
+        // USAR VARIAVEIS
+        var insertSql = @"INSERT INTO
+                    [Category]
+                VALUES(
+                    @Id,
+                    @Title,
+                    @Url,
+                    @Summary,
+                    @Order,
+                    @Description,
+                    @Featured)";
+
+        var rows = connection.Execute(insertSql, new
+        {
+            category.Id,
+            category.Title,
+            category.Url,
+            category.Summary,
+            category.Order,
+            category.Description,
+            category.Featured
+        });
+        Console.WriteLine($"{rows} linhas inseridas.");
+    }
+
+    static void UpdateCategory(SqlConnection connection)
+    {
+        var updateQuery = "UPDATE [Category] SET [Title]=@title WHERE [Id]=@id";
+        var rows = connection.Execute(updateQuery, new
+        {
+            id = new Guid("7dc10bdc-ee85-4179-8d02-652c586e8efb"),
+            title = "Amazon AWS",
+        });
+        Console.WriteLine($"{rows} registros atualizados.");
+    }
+}
+
+// install ado net 'dotnet add package Microsoft.Data.SqlClient'
